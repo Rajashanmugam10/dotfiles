bfc8315d416b7c2e1ededbd6b07f9d498a97c65c
Rajashanmugam10 <rajashanmugam1010@gmail.com>
2025-08-05 20:57:39 +0530

    re-built

--- config.fish
+++ config.fish
@@ -1,16 +1,12 @@
 set -U fish_greeting ""
-set -g fish_color_normal e0def4
-set -g fish_color_command 9ccfd8
-set -g fish_color_param f6c177
-set -g fish_color_comment 6e6a86
-set -g fish_color_error eb6f92
 
 alias n="nvim"
 alias b="sudo brightnessctl set"
 alias v="wpctl set-volume @DEFAULT_AUDIO_SINK@"
 alias po="sudo poweroff"
 alias rb="sudo reboot"
-
+alias t="tmux"
+alias l='eza --icons --no-permissions --no-time --long'
 alias pi="sudo pacman -S"
 alias search="sudo pacman -Ss"
 alias psy="sudo pacman -Syu"
@@ -24,7 +20,10 @@ alias nsw="nvim ~/.config/dotfiles/sway/config"
 alias nft="nvim ~/.config/dotfiles/foot/foot.ini"
 alias ntm="nvim ~/.config/dotfiles/tmux.conf"
 alias nfc="nvim ~/.config/dotfiles/config.fish"
+
 zoxide init fish | source
 
-df /dev/mapper/e-disk -h | awk 'NR==2 {print $4}'
-acpi -b
+#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#sudo mkdir -p /run/user/1000
+#sudo chown $USER:$USER /run/user/1000
+#sudo chmod 0700 /run/user/1000
